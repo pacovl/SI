e9d94a69f223c84c7eb2daaf9d5b3123477362a1
Paco <curro258@gmail.com>
2018-10-24 20:15:01 +0200

    cosas varias

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,19 +2,22 @@ import os
 import hashlib
 import json
 import functools
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, make_response
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from random import randint
 
 
 def create_app(test_config=None):
+    sess = Session()
+
     CUR_DIR = os.getcwd()
     print(CUR_DIR)
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.secret_key = os.urandom(24)
+    sess.init_app(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -33,7 +36,7 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    #Aqui obtenemos el catalogo y las categorias
+    # Aqui obtenemos el catalogo y las categorias
     with open('catalogo.json') as f:
         catalogo = json.load(f)
         categorias = ["--"]
@@ -42,10 +45,10 @@ def create_app(test_config=None):
                 if(cat and (cat not in categorias)):
                     categorias.append(cat)
 
-    #Definimos index.html
+    # Definimos index.html
     @app.route('/', methods=['POST', 'GET'])
     def index():
-        #Identificamos solicitudes post tras busqueda
+        # Identificamos solicitudes post tras busqueda
         if request.method == 'POST':
             type = request.form.keys()
 
@@ -53,7 +56,7 @@ def create_app(test_config=None):
                 search = request.form['seleccion']
                 #pelis = catalogo["peliculas"].filter(lambda x: x["titulo"] == filmname)
                 #category = request.form['categoria']
-                #print(category)
+                # print(category)
 
                 lista_filtrada = []
                 for pelicula in catalogo['peliculas']:
@@ -62,17 +65,16 @@ def create_app(test_config=None):
 
                 indice = int(categorias.index(search))
 
-                user = session.get('user')
-
-                return render_template('index.html', seleccion = lista_filtrada[:9], cats = categorias, user_id = user)
+                return render_template('index.html', seleccion=lista_filtrada[:9], cats=categorias)
 
             if "username" in type:
 
-                #Recibimos los campos de registro
+                # Recibimos los campos de registro
                 nombre = request.form['username']
-                password = hashlib.md5(request.form['password'].encode('utf8')).hexdigest()
+                password = hashlib.md5(
+                    request.form['password'].encode('utf8')).hexdigest()
 
-                #Comprobamos si existe una carpeta con el mismo nombre
+                # Comprobamos si existe una carpeta con el mismo nombre
                 dir_name = CUR_DIR + '/usuarios/' + nombre
                 if (not os.path.isdir(dir_name)):
                     return "No existe ese usuario"
@@ -83,61 +85,58 @@ def create_app(test_config=None):
                 if password != datos_usuario["password"]:
                     return "Contrasenia incorrecta"
 
-                #Creamos la cookie de session
-                session['user'] = datos_usuario["nombre"]
-                session.modified = True
-
-                resp = make_response(render_template('index.html', seleccion = catalogo["peliculas"][:9], cats = categorias, user_id = datos_usuario["nombre"]))
-                #resp.set_cookie('user_id', datos_usuario["nombre"])
-
-                return resp
+                # TODO Hacer el login
+                print("LOGIN EXITOSOOOOOOOO")
+                return render_template('index.html', seleccion=catalogo["peliculas"][:9], cats=categorias)
             if "fnombre" in type:
 
-                #Recibimos los campos de registro
+                # Recibimos los campos de registro
                 nombre = request.form['fnombre']
-                password = hashlib.md5(request.form['fpass'].encode('utf8')).hexdigest()
+                password = hashlib.md5(
+                    request.form['fpass'].encode('utf8')).hexdigest()
                 email = request.form['femail']
                 card = request.form['fcard']
                 sex = request.form['fsex']
                 saldo = randint(0, 100)
 
-                #Creamos un dict con los campos de registro
-                dict = {'nombre': nombre, 'password': password, 'email': email, 'card': card, 'sex': sex, 'saldo': saldo}
+                # Creamos un dict con los campos de registro
+                dict = {'nombre': nombre, 'password': password,
+                        'email': email, 'card': card, 'sex': sex, 'saldo': saldo}
 
-                #Comprobamos si existe una carpeta con el mismo nombre
+                # Comprobamos si existe una carpeta con el mismo nombre
                 dir_name = CUR_DIR + '/usuarios/' + nombre
                 if (not os.path.isdir(dir_name)):
                     os.makedirs(dir_name)
-                    #Escribimos un archivo json con el usuario
+                    # Escribimos un archivo json con el usuario
                     with open(dir_name + '/datos.json', 'w+') as outfile:
                         json.dump(dict, outfile)
                 else:
                     return "El usuario ya existe"
 
-                return render_template('index.html', seleccion = catalogo["peliculas"][:9], cats = categorias, user_id = session.get('user'))
+                return render_template('index.html', seleccion=catalogo["peliculas"][:9], cats=categorias)
 
             if "buscar" in type:
                 search = request.form['buscar']
                 #pelis = catalogo["peliculas"].filter(lambda x: x["titulo"] == filmname)
                 #category = request.form['categoria']
-                #print(category)
+                # print(category)
 
                 lista_filtrada = []
                 for pelicula in catalogo['peliculas']:
                     if pelicula["titulo"].lower().find(search.lower()) != -1:
                         lista_filtrada.append(pelicula)
 
-                return render_template('index.html', seleccion = lista_filtrada[:9], cats = categorias,user_id = session.get('user'))
+                return render_template('index.html', seleccion=lista_filtrada[:9], cats=categorias)
 
-        #Pasamos la lista de peliculas para obtener los datos en seleccion
-        return render_template('index.html', seleccion = catalogo["peliculas"][:9], cats = categorias, user_id = session.get('user'))
+        # Pasamos la lista de peliculas para obtener los datos en seleccion
+        return render_template('index.html', seleccion=catalogo["peliculas"][:9], cats=categorias)
 
     @app.route('/detalle', methods=['POST', 'GET'])
     def detalle():
         pelicula = request.args.get('pelicula')
         for peli in catalogo["peliculas"]:
             if peli['titulo'] == pelicula:
-                return render_template('detalle.html', seleccion=peli, recomendadas=catalogo["peliculas"][:5], cats = categorias, user_id = session.get('user'))
+                return render_template('detalle.html', seleccion=peli, recomendadas=catalogo["peliculas"][:3], cats=categorias)
 
         return "No se ha encontrado la pelicula"
 
@@ -147,9 +146,10 @@ def create_app(test_config=None):
 
     @app.route('/carrito', methods=['POST', 'GET'])
     def carrito():
-        return render_template('carrito.html', seleccion = catalogo['peliculas'][:4], user_id = session.get('user'))
+        return render_template('carrito.html', seleccion=catalogo['peliculas'][:4])
 
     return app
+
 
 if __name__ == '__main__':
     app = create_app()
